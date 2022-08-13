4833fb7a5b41fb8ab25422c49e4a338d44055793
vahid4 <vahid.gasanin1gmail.com>
2022-08-13 22:09:10 +0200

    commit

--- Desktop/my-app/src/App.jsx
+++ Desktop/my-app/src/App.jsx
@@ -5,15 +5,9 @@ import {Link, Route, Routes} from "react-router-dom"
 function App() {
   return (
     <div>
-      <Link to={"/"}  />
-    
-    <Routes>
-<Route path={"/"} element={<h1>nesto</h1>} />
-<Route path='c' element={<h2>home</h2>}/>
+      
 
-    </Routes>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sapiente eos excepturi explicabo at ab nobis perspiciatis optio, labore culpa necessitatibus sed. Suscipit iste fugit, ad iusto temporibus dolores laudantium.</p>
-
+      
     </div>
   );
 }
